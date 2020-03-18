58421e1a9fd9c4bb51096f73c5498f670da8c8ea
davism9902 <62302962+davism9902@users.noreply.github.com>
2020-03-18 12:09:46 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_DavisMaia.py
@@ -0,0 +1,15 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: D:/FTCC/P2T1_SalesPrediction_DavisMaia.py ==============
+# practice using the print and input functions
+# 3/18/20
+# CTI-110 P2T1 - Sales Prediction
+# Maia Davis
+>>> 
+============== RESTART: D:/FTCC/P2T1_SalesPrediction_DavisMaia.py ==============
+# practice using the print and input functions
+# 3/18/20
+# CTI-110 P2T1 - Sales Prediction
+# Maia Davis
+Enter the projected sales: 
